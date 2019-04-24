310bec6de7470a05c1d6df7711ad6a908ac60ee0
Manie <manassehl9@gmail.com>
2019-04-24 13:19:35 +0100

    categorized the project tasks

--- src/components/ProjectBoard/ProjectBoard.js
+++ src/components/ProjectBoard/ProjectBoard.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Backlog from './Backlog';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getBacklog } from '../../actions/backlogActions';
 
 class ProjectBoard extends Component {
+	// constructor to handle errors
+
+	componentDidMount() {
+		//extract from the route
+		const { id } = this.props.match.params;
+
+		this.props.getBacklog(id);
+	}
+
 	render() {
 		const { id } = this.props.match.params;
+		const { project_tasks } = this.props.backlog;
 		return (
 			<div className="container">
 				<Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -13,10 +26,19 @@ class ProjectBoard extends Component {
 				<br />
 				<hr />
 
-				<Backlog />
+				<Backlog project_tasks_prop={project_tasks} />
 			</div>
 		);
 	}
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+	backlog: PropTypes.object.isRequired,
+	getBacklog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	backlog: state.backlog
+});
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
